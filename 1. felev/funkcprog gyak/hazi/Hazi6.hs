29384a192ad7b45e335d6286ac6d67aae94881e5
csabisoos <csabi.soos.05@gmail.com>
2024-10-21 16:23:39 +0200

    haskell hazik

---
+++ 1. felev/funkcprog gyak/hazi/Hazi6.hs
@@ -0,0 +1,25 @@
+module Hazi6 where
+
+toUpperThird :: String -> String
+toUpperThird = undefined
+
+isSorted :: Ord a => [a] -> Bool
+isSorted = undefined
+
+(!!!) :: Integral b => [a] -> b -> a
+(!!!) = undefined
+
+format :: Integral b => b -> String -> String
+format = undefined
+
+mightyGale :: (Num a, Ord b, Num b, Integral c) => [(String, a, b, c)] -> String
+mightyGale = undefined
+
+-- cipher :: 
+-- cipher = undefined
+
+doubleElements :: [a] -> [a]
+doubleElements = undefined
+
+deleteDuplicateSpaces :: String -> String
+deleteDuplicateSpaces = undefined
