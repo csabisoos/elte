9653731c2f149a14a04a8f50ca76bacebca83027
csabisoos <csabi.soos.05@gmail.com>
2024-10-24 13:05:33 +0200

    beadando 2.fazis

--- 1. felev/programozas gyak/beadando_1/beadando_1/beadando_1/Program.cs
+++ 1. felev/programozas gyak/beadando_1/beadando_1/beadando_1/Program.cs
@@ -1,53 +1,52 @@
-﻿using System;
-
-//namespace beadando_1;
-
-class Program
+﻿/** /
+Név: Soós Csaba
+Neptun kód: AZXX1Z
+Email cím: azxx1z@inf.elte.hu
+           sooscsaba@student.elte.hu
+/**/
+using System;
+namespace beadando_1
 {
-    struct Varos
+    class Program
     {
-        public int tav;
-        public int ar;
-    };
-    static void Main(string[] args)
-    {
-        int n = 0;
-        int.TryParse(Console.ReadLine(), out n);
-        Varos[] varosok = new Varos[n];
-        string sor;
-        //int maxind = 0;
-        int maxert;
-        int minar;
-        for (int i = 0; i < n; i++)
+        struct Varos
         {
-            sor = Console.ReadLine();
-            int.TryParse(sor.Split(' ')[0], out varosok[i].tav);
-            int.TryParse(sor.Split(' ')[1], out varosok[i].ar);
-        }
-
-        maxert = varosok[0].tav;
-        minar = varosok[0].ar;
-
-        for (int i = 1; i < n; i++)
+            public int tav;
+            public int ar;
+        };
+        static void Main(string[] args)
         {
-            /** /
-            if (maxert < varosok[i].tav)
+            int n = 0;
+            int.TryParse(Console.ReadLine(), out n);
+            Varos[] varosok = new Varos[n];
+            string sor;
+            int maxtav;
+            int minar;
+
+            for (int i = 0; i < n; i++)
             {
-                maxert = varosok[i].tav;
-                maxind = i;
+                sor = Console.ReadLine();
+                int.TryParse(sor.Split(' ')[0], out varosok[i].tav);
+                int.TryParse(sor.Split(' ')[1], out varosok[i].ar);
             }
-            /**/
-            /**/
-            if (maxert < varosok[i].tav){
-                maxert = varosok[i].tav;
-                minar = varosok[i].ar;
+
+            maxtav = varosok[0].tav;
+            minar = varosok[0].ar;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (maxtav < varosok[i].tav)
+                {
+                    maxtav = varosok[i].tav;
+                    minar = varosok[i].ar;
+                }
+                else if (maxtav == varosok[i].tav && minar > varosok[i].ar)
+                {
+                    minar = varosok[i].ar;
+                }
             }
-            else if (maxert == varosok[i].tav && minar > varosok[i].ar){
-                minar = varosok[i].ar;
-            }
-            /**/
+
+            Console.WriteLine(minar);
         }
-        //Console.WriteLine(varosok[maxind].ar);
-        Console.WriteLine(minar);
     }
 }
