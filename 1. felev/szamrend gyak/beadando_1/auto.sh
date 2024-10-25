572d9db0e15faaf40e82dfdc3cf53c17649270db
csabisoos <csabi.soos.05@gmail.com>
2024-10-26 00:12:24 +0200

    szamrend

--- 1. felev/szamrend gyak/beadando_1/auto.sh
+++ 1. felev/szamrend gyak/beadando_1/auto.sh
@@ -4,4 +4,9 @@ file="test.txt"
 last_time=""
 stop_count=0
 
+while read line || [[ -n $line ]]
+do
+    IFS=' ' $line datum ido elol hatul
+    
+done
 echo "Megállási események száma: $stop_count"
