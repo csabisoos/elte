2cfddba6283e1e8188ae91d1892a313d608143c1
csabisoos <csabi.soos.05@gmail.com>
2024-10-25 23:42:39 +0200

    szamrend beadando kezdes

---
+++ 1. felev/szamrend gyak/beadando_1/fekez.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# K check
+if [[ $# -ne 1 ]]; then
+    echo "Használat: ./fekez.sh <távolság_méterben>"
+    exit 1
+fi
+
+K=$1
+
+# fajl neve
+file="test.txt"
+
+utolso_idopont=""
+megallas=0
+
+while read -r line; do
+    
