3f0ec9c962b14fc86e83a24de1c77f959987e734
csabisoos <csabi.soos.05@gmail.com>
2024-11-24 20:07:28 +0100

    beadandok

---
+++ 1. felev/funkcprog gyak/beadando/BeadSkeleton.hs
@@ -0,0 +1,102 @@
+module NagyBead where
+
+import Data.Either
+import Data.Maybe
+
+basicInstances = 0 -- Mágikus tesztelőnek kell ez, NE TÖRÖLD!
+
+type OperatorTable a = [(Char, (a -> a -> a, Int, Dir))]
+
+tAdd, tMinus, tMul, tDiv, tPow :: (Floating a) => Tok a
+tAdd = TokBinOp (+) '+' 6 InfixL
+tMinus = TokBinOp (-) '-' 6 InfixL
+tMul = TokBinOp (*) '*' 7 InfixL
+tDiv = TokBinOp (/) '/' 7 InfixL
+tPow = TokBinOp (**) '^' 8 InfixR
+
+operatorTable :: (Floating a) => OperatorTable a
+operatorTable =
+    [ ('+', ((+), 6, InfixL))
+    , ('-', ((-), 6, InfixL))
+    , ('*', ((*), 7, InfixL))
+    , ('/', ((/), 7, InfixL))
+    , ('^', ((**), 8, InfixR))
+    ]
+
+--getOp :: (Floating a) => Char -> Maybe (Tok a)
+--getOp = operatorFromChar operatorTable
+--
+--parse :: String -> Maybe [Tok Double]
+--parse = parseTokens operatorTable
+--
+--parseAndEval :: (String -> Maybe [Tok a]) -> ([Tok a] -> ([a], [Tok a])) -> String -> Maybe ([a], [Tok a])
+--parseAndEval parse eval input = maybe Nothing (Just . eval) (parse input)
+--
+--syNoEval :: String -> Maybe ([Double], [Tok Double])
+--syNoEval = parseAndEval parse shuntingYardBasic
+--
+--syEvalBasic :: String -> Maybe ([Double], [Tok Double])
+--syEvalBasic = parseAndEval parse (\t -> shuntingYardBasic $ BrckOpen : (t ++ [BrckClose]))
+--
+--syEvalPrecedence :: String -> Maybe ([Double], [Tok Double])
+--syEvalPrecedence = parseAndEval parse (\t -> shuntingYardPrecedence $ BrckOpen : (t ++ [BrckClose]))
+
+-- eqError-t vedd ki a kommentből, ha megcsináltad az 1 pontos "Hibatípus definiálása" feladatot
+-- eqError = 0 -- Mágikus tesztelőnek szüksége van rá, NE TÖRÖLD!
+
+{-
+-- Ezt akkor vedd ki a kommentblokkból, ha a 3 pontos "A parser és az algoritmus újradefiniálása" feladatot megcsináltad.
+parseAndEvalSafe ::
+    (String -> ShuntingYardResult [Tok a]) ->
+    ([Tok a] -> ShuntingYardResult ([a], [Tok a])) ->
+    String -> ShuntingYardResult ([a], [Tok a])
+parseAndEvalSafe parse eval input = either Left eval (parse input)
+
+sySafe :: String -> ShuntingYardResult ([Double], [Tok Double])
+sySafe = parseAndEvalSafe
+  (parseSafe operatorTable)
+  (\ts -> shuntingYardSafe (BrckOpen : ts ++ [BrckClose]))
+-}
+
+{-
+-- Ezt akkor vedd ki a kommentblokkból, ha az 1 pontos "Függvénytábla és a típus kiegészítése" feladatot megcsináltad.
+tSin, tCos, tLog, tExp, tSqrt :: Floating a => Tok a
+tSin = TokFun sin "sin"
+tCos = TokFun cos "cos"
+tLog = TokFun log "log"
+tExp = TokFun exp "exp"
+tSqrt = TokFun sqrt "sqrt"
+
+functionTable :: (RealFrac a, Floating a) => FunctionTable a
+functionTable =
+    [ ("sin", sin)
+    , ("cos", cos)
+    , ("log", log)
+    , ("exp", exp)
+    , ("sqrt", sqrt)
+    , ("round", (\x -> fromIntegral (round x :: Integer)))
+    ]
+-}
+
+{-
+-- Ezt akkor vedd ki a kommentblokkból, ha a 2 pontos "Függvények parse-olása és kiértékelése" feladatot megcsináltad.
+syFun :: String -> Maybe ([Double], [Tok Double])
+syFun = parseAndEval
+  (parseWithFunctions operatorTable functionTable)
+  (\t -> shuntingYardWithFunctions $ BrckOpen : (t ++ [BrckClose]))
+-}
+
+{-
+-- Ezt akkor vedd ki a kommentblokkból, ha minden más feladatot megcsináltál ez előtt.
+syComplete :: String -> ShuntingYardResult ([Double], [Tok Double])
+syComplete = parseAndEvalSafe
+  (parseComplete operatorTable functionTable)
+  (\ts -> shuntingYardComplete (BrckOpen : ts ++ [BrckClose]))
+-}
+
+---------------------------------
+
+data Dir = InfixR | InfixL
+  deriving (Show, Eq, Ord)
+
+data Tok a = BrckOpen | BrckClose | TokLit a | TokBinOp (a -> a -> a) Char Int Dir
