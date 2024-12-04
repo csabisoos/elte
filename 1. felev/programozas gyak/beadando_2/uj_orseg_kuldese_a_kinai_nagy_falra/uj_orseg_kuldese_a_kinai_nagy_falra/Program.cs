7a2cc26133e330220d2f33481a084e7b2cd0e706
csabisoos <csabi.soos.05@gmail.com>
2024-12-04 01:09:19 +0100

    nap vege

---
+++ 1. felev/programozas gyak/beadando_2/uj_orseg_kuldese_a_kinai_nagy_falra/uj_orseg_kuldese_a_kinai_nagy_falra/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace uj_orseg_kuldese_a_kinai_nagy_falra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sortomb = Console.ReadLine().Split(' ');
+            int n = int.Parse(sortomb[0]);
+            int m = int.Parse(sortomb[1]);
+            int[] lista = new int[m];
+            for (int i = 0; i < m; i++)
+            {
+                lista[i] = (int.Parse(Console.ReadLine()));
+            }
+            int[] orsegek = new int[n];
+            for (int i = 0; i < m; i++)
+            {
+                orsegek[lista[i]-1] = 1;
+            }
+            foreach (int elem in lista)
+            {
+                orsegek[elem] = 1;
+            }
+            
+        }
+    }
+}
