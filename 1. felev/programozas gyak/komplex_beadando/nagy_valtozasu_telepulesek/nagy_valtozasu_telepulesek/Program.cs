ca4d50b97953d023ad73e8a2db7693bfad00b48c
csabisoos <csabi.soos.05@gmail.com>
2025-01-07 00:43:29 +0100

    prog komplex bead dokumentacio felig kesz

--- 1. felev/programozas gyak/komplex_beadando/nagy_valtozasu_telepulesek/nagy_valtozasu_telepulesek/Program.cs
+++ 1. felev/programozas gyak/komplex_beadando/nagy_valtozasu_telepulesek/nagy_valtozasu_telepulesek/Program.cs
@@ -4,12 +4,23 @@ namespace nagy_valtozasu_telepulesek
 {
     class Program
     {
-        static void Main(string[] args)
+        static int[,] beolvas()
+        {
+            if (Console.IsInputRedirected)
+            {
+                return beolvas_biro();
+            }
+            else
+            {
+                return beolvas_kezi();
+            }
+        }
+        static int[,] beolvas_biro()
         {
             string[] sortomb = Console.ReadLine().Split(' ');
             int n = int.Parse(sortomb[0]);
             int m = int.Parse(sortomb[1]);
-            int[,] homerseklet = new int[n,m];
+            int[,] homerseklet = new int[n, m];
 
             for (int i = 0; i < n; i++)
             {
@@ -19,19 +30,74 @@ namespace nagy_valtozasu_telepulesek
                     homerseklet[i, j] = int.Parse(sortomb[j]);
                 }
             }
-            int db=0;
-            List<int> sorszamok = new List<int>();
+            return homerseklet;
+        }
+        static int[,] beolvas_kezi()
+        {
+            int n, m;
+            bool jo;
+            do
+            {
+                Console.ResetColor();
+                Console.Write("Települések száma = ");
+                jo = int.TryParse(Console.ReadLine(), out n) && 1<=n && n<=1000;
+                if (!jo)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("A szám nem megfelelő (1<=N<=1000)!");
+                }
+            } while (!jo);
+            do
+            {
+                Console.ResetColor();
+                Console.Write("Napok száma = ");
+                jo = int.TryParse(Console.ReadLine(), out m) && 1 <= m && m <= 1000;
+                if (!jo)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("A szám nem megfelelő (1<=N<=1000)!");
+                }
+            } while (!jo);
+
+            int[,] homerseklet = new int[n,m];
             for (int i = 0; i < n; i++)
             {
+                for (int j = 0; j < m; j++)
+                {
+                    do
+                    {
+                        Console.ResetColor();
+                        Console.Write($"{i + 1}. település {j + 1}. hömérséklet értéke = ");
+                        jo = int.TryParse(Console.ReadLine(), out homerseklet[i, j]) && -50 <= homerseklet[i, j] && homerseklet[i, j] <= 50;
+                        if (!jo)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("A szám nem megfelelő (-50<=H<=50)");
+                        }
+                    } while (!jo);
+                }
+            }
+            return homerseklet;
+        }
+        static void Main(string[] args)
+        {
+            int[,] homerseklet;
+            int db = 0;
+            List<int> sorszamok = new List<int>();
+
+            homerseklet = beolvas();
+
+            for (int i = 0; i < homerseklet.GetLength(0); i++)
+            {
                 int j = 1;
-                while (j<m && !(homerseklet[i, j - 1] - homerseklet[i,j]>=10 || homerseklet[i, j] - homerseklet[i,j-1]>=10))
+                while (j < homerseklet.GetLength(1) && !(homerseklet[i, j - 1] - homerseklet[i, j] >= 10 || homerseklet[i, j] - homerseklet[i, j - 1] >= 10))
                 {
                     j += 1;
                 }
-                if (j<m)
+                if (j < homerseklet.GetLength(1))
                 {
                     db += 1;
-                    sorszamok.Add(i+1);
+                    sorszamok.Add(i + 1);
                 }
             }
             Console.Write($"{db} ");
@@ -39,3 +105,9 @@ namespace nagy_valtozasu_telepulesek
         }
     }
 }
+/*
+3 5
+10 19 12 10 10
+1 11 11 11 20
+12 16 16 16 -2
+ */
